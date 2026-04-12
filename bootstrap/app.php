e5de6e22e158efeec7f52a62e1bb69be28c77cbf
Ben Potter <me@bpmct.net>
2026-04-12 14:52:47 +0000

    fix: register TrustProxies middleware from trustedproxy config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            at: config('trustedproxy.proxies'),
+        );
         $middleware->alias([
             'abilities' => CheckAbilities::class,
             'ability' => CheckForAnyAbility::class,
